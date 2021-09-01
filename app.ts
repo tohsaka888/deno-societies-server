07f59a9ca9684a8339fe03bb5efbaa19794c52ba
tohsaka888 <156132264@qq.com>
2021-09-01 12:37:19 +0800

    test

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 import {
   create,
   verify,
